ba022f6d7a01ee139f206647b52a53d84ed2528c
Pavan Raibagi <134160998+pavantech01@users.noreply.github.com>
2025-03-17 22:32:15 +0530

    api js updated

--- client-ashokafae/src/utils/api.js
+++ client-ashokafae/src/utils/api.js
@@ -1,2 +1,2 @@
-// export const Base_URL = 'http://localhost:5000';  // Localhost URL
-export const API_URL = 'https://ashokafae-backend.onrender.com';
+export const Base_URL = 'http://localhost:5000';  // Localhost URL
+// export const API_URL = 'https://ashokafae-backend.onrender.com';
